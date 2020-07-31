6401bac71904da83f8f0593f94c10f9423b65c67
HAN SEUNGHO <default>
2020-07-31 14:11:27 +0900

    Update SKIOLIB.

--- include/SKIOLib.hh
+++ include/SKIOLib.hh
@@ -1,6 +1,7 @@
 #ifndef SKIOLIB_HH
 #define SKIOLIB_HH 1
 
+// SK I/O
 extern "C" {
     void kzinit_();
     void geoset_();
@@ -11,6 +12,26 @@ extern "C" {
     void skoptn_(const char*, int);
     int skread_(int*);
     void skclosef_(int*);
+}
+
+// bank control
+extern "C" {
+    void nerdnebk_(float*);
+    void skgetv_();
+    void apflscndprt_();
+    void trginfo_(float*);
+    void aprstbnk_(int*);
+    void odpc_2nd_s_(int*);
+    void inpmt_(float*, int&);
+}
+
+// BONSAI
+extern "C" {
+    void bonsai_ini_();
+    void bonsai_fit_(float*, float*, float*, int*, int*, float*, float*, float*, 
+                     float*, float*, float*, float*, float*, float*);
+    void bonsai_end_();
+    void bonsai_energy_(float*, float*, float*, float*, int*, int*, float*);
 }
 
 #endif
