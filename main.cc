439105d029bf6ff801cb2c28a1343c6bdf3e5a2a
HAN SEUNGHO <default>
2020-08-21 14:46:02 +0900

    Fix tabs.

--- main.cc
+++ main.cc
@@ -6,23 +6,23 @@
 
 int main(int argc, char** argv)
 {
-	std::string inputName, outputName;
-	
-	if (argc == 1) {
-	    std::cerr << "[NTag] Usage: ./bin/main [output name] [input file]" << std::endl;
-	}
-	else if (argc == 2) {
-	    // argv 1    : input file
-	    outputName = "out/NTagOut.root";
-	    inputName  = argv[1];
-	}
-	else if (argc > 2) {
-	    // argv 1    : output file
-	    // argv 2 ~  : input file ...
+    std::string inputName, outputName;
+
+    if (argc == 1) {
+        std::cerr << "[NTag] Usage: ./bin/main [output name] [input file]" << std::endl;
+    }
+    else if (argc == 2) {
+        // argv 1    : input file
+        outputName = "out/NTagOut.root";
+        inputName  = argv[1];
+    }
+    else if (argc > 2) {
+        // argv 1    : output file
+        // argv 2 ~  : input file ...
         outputName = argv[1];
         inputName  = argv[2];
-	}
-    
+    }
+
     if (TString(inputName).EndsWith(".root")) {
         NTagROOT nt(inputName.c_str(), outputName.c_str(), true, pDEBUG);
         nt.SetCustomVertex(-400, 0, 0);
@@ -34,6 +34,6 @@ int main(int argc, char** argv)
         nt.Initialize();
         nt.PrintMessage("Done!");
     }
-    
+
     return 0;
 }
