0048605ea4de1bc53ecbc4441cc4222c2b94876e
HAN SEUNGHO <default>
2020-09-22 09:38:18 +0900

    Cd back to sphinx/source.

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -22,7 +22,7 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
 
-    subprocess.call('cd ../../doxygen; doxygen Doxyfile', shell=True)
+    subprocess.call('cd ../../doxygen; doxygen Doxyfile; cd ../sphinx/source', shell=True)
 
 # -- Project information -----------------------------------------------------
 
