06feb81cdd0eef2b0de937a29328469db20d56ac
HAN SEUNGHO <default>
2020-09-22 09:15:43 +0900

    Update conf.py.

--- docs/sphinx/source/conf.py
+++ docs/sphinx/source/conf.py
@@ -22,19 +22,17 @@ read_the_docs_build = os.environ.get('READTHEDOCS', None) == 'True'
 
 if read_the_docs_build:
 
-    subprocess.call('cd ../doxygen; doxygen', shell=True)
+    subprocess.call('doxygen ../doxygen/Doxyfile', shell=True)
 
 # -- Project information -----------------------------------------------------
 
 project = u'NTag'
 copyright = u'2020, The Super-Kamiokande Collaboration'
-author = u'Seungho Han, Masayuki Harada'
 
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
 release = u'0.0.1'
-
 
 # -- General configuration ---------------------------------------------------
 
