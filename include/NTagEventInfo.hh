9f90fe0164d889c8045aa0a6516cf9f72107c06b
HAN SEUNGHO <default>
2020-11-09 11:51:11 +0900

    Update default values.

--- include/NTagEventInfo.hh
+++ include/NTagEventInfo.hh
@@ -72,7 +72,7 @@ namespace NTagConstant{
 * @brief Default parameters used in NTag.
 *******************************************/
 namespace NTagDefault{
-    constexpr float TWIDTH       = 13.;   ///< Default value for NTagEventInfo::TWIDTH.
+    constexpr float TWIDTH       = 14.;   ///< Default value for NTagEventInfo::TWIDTH.
     constexpr int   NHITSTH      = 7;     ///< Default value for NTagEventInfo::NHITSTH.
     constexpr int   NHITSMX      = 70;    ///< Default value for NTagEventInfo::NHITSMX.
     constexpr int   N200MX       = 200;   ///< Default value for NTagEventInfo::N200MX.
@@ -83,7 +83,7 @@ namespace NTagDefault{
     constexpr float TMINPEAKSEP  = 50.;   ///< Default value for NTagEventInfo::TMINPEAKSEP. (ns)
     constexpr int   ODHITMX      = 16;    ///< Default value for NTagEventInfo::ODHITMX.
     constexpr float TRBNWIDTH    = 3.;    ///< Default value for NTagEventInfo::TRBNWIDTH. (us)
-    constexpr float PVXRES       = 20.;    ///< Default value for NTagEventInfo::PVXRES. (cm)
+    constexpr float PVXRES       = 20.;   ///< Default value for NTagEventInfo::PVXRES. (cm)
 }
 
 /**********************************************************
