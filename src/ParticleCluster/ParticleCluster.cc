a1c522b7fa787da5f6ce5c954c8a0cf5325fe577
HAN SEUNGHO <default>
2021-08-19 11:31:26 +0900

    Add ParticleCluster constructor for common.

--- src/ParticleCluster/ParticleCluster.cc
+++ src/ParticleCluster/ParticleCluster.cc
@@ -1,7 +1,47 @@
 #include <iomanip>
 
+#include <neworkC.h>
+
+#include "SKLibs.hh"
 #include "Calculator.hh"
 #include "ParticleCluster.hh"
+
+ParticleCluster::ParticleCluster(vcwork_common primaryCommon, secndprt_common secondaryCommon)
+{
+    // Primaries
+    for (int iVec = 0; iVec < primaryCommon.nvect; iVec++) {
+        int g3pid = skvect_.ip[iVec];
+        int g4pid = 0;
+        if (g3pid == 4) {
+            // get pid from neut vectors
+            float posnu[3]; nerdnebk_(posnu);
+            g4pid = nework_.ipne[0];
+        }
+        else g4pid = gG3toG4PIDMap[g3pid];
+        Particle primary(g4pid, 0, TVector3(skvect_.pos), TVector3(skvect_.pin[iVec]));
+        Append(primary);
+    }
+    
+    // Secondaries
+    int nAllSec = secondaryCommon.nscndprt;
+    for (int iSec = 0; iSec < nAllSec; iSec++) {
+
+        Particle secondary(secondaryCommon.iprtscnd[iSec],
+                           secondaryCommon.tscnd[iSec],
+                           TVector3(secondaryCommon.vtxscnd[iSec]),
+                           TVector3(secondaryCommon.pscnd[iSec]),
+                           secondaryCommon.iprntprt[iSec],
+                           secondaryCommon.lmecscnd[iSec]);
+
+        Append(secondary);
+    }
+}
+
+void ParticleCluster::SetT0(float t0)
+{
+    for (auto& particle: fElement)
+        particle.AddT0(t0);
+}
 
 void ParticleCluster::Sort()
 {
--- src/ParticleCluster/ParticleCluster.hh
+++ src/ParticleCluster/ParticleCluster.hh
@@ -1,6 +1,9 @@
 #ifndef PARTICLECLUSTER_HH
 #define PARTICLECLUSTER_HH
 
+#include <skvectC.h>
+#include <apscndryC.h>
+
 #include "Particle.hh"
 #include "Cluster.hh"
 
@@ -8,6 +11,9 @@ class ParticleCluster : public Cluster<Particle>
 {
     public:
         ParticleCluster() {}
+        ParticleCluster(vcwork_common primary, secndprt_common secondary);
+        
+        void SetT0(float t0);
         
         void Sort();
         void DumpAllElements();
