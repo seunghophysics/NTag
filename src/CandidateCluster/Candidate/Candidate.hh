c2787ad8f57a0decb5cf042c6bd9d1e77592e83a
HAN SEUNGHO <default>
2021-08-30 17:20:44 +0900

    Add candidate truelabel enum, etc.

--- src/CandidateCluster/Candidate/Candidate.hh
+++ src/CandidateCluster/Candidate/Candidate.hh
@@ -6,19 +6,30 @@
 
 //#include "Rtypes.h"
 
+enum TrueLabel
+{
+    lNoise,
+    lDecayE,
+    lnH,
+    lnGd,
+    lRemnant,
+    lUndefined
+};
+
 class Candidate
 {
     public:
         Candidate(const unsigned int iHit=0);
 
         inline unsigned int HitID() const { return hitID; }
+        inline void SetHitID(unsigned int id) { hitID = id; }
         
-        float& operator[](const std::string& key) { return featureMap[key];}
+        const float operator[](const std::string& key) const { return featureMap.at(key); }
         void Set(const std::string& key, float value) { featureMap[key] = value; }
-        float& Get(const std::string& key) { return featureMap[key];}
+        const float Get(const std::string& key) const { return featureMap.at(key); }
         void Clear() { featureMap.clear(); }
 
-        std::map<std::string, float>& GetFeatureMap() { return featureMap; }
+        const std::map<std::string, float>& GetFeatureMap() const { return featureMap; }
 
     private:
         std::map<std::string, float> featureMap;
