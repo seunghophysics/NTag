ddb8f5e771f08b111bddb0fac86f4293528ce489
HAN SEUNGHO <default>
2021-11-01 13:52:37 +0900

    TrgSim: time seeding done.

--- src/TrgSim/TrgSim.cc
+++ src/TrgSim/TrgSim.cc
@@ -1,51 +1,50 @@
 #include <iostream>
 
 #include "TChain.h"
+#include "TRandom3.h"
+#include "TUnuran.h"
 
 #include "TrgSim.hh"
 
-TrgSim::TrgSim() : 
-fSigFreqHz(0), fTDurationSec(0),
-fSignalTree(nullptr), fNoiseTree(nullptr), 
-fSignalIDTQ(nullptr), fNoiseIDTQ(nullptr), fNoiseODTQ(nullptr) 
+TrgSim::TrgSim(Verbosity verbose) : 
+fSigFreqHz(0), fTDurationSec(0), fRandomSeed(0),
+fSignalTree(nullptr), fNoiseTree(nullptr),
+fCurrentSignalEntry(0), fCurrentNoiseEntry(0),
+fTotalSignalEntries(0), fTotalNoiseEntries(0),
+fSignalIDTQ(nullptr), fNoiseIDTQ(nullptr), fNoiseODTQ(nullptr),
+fSegment(), fSegmentLength(10e-3 /* 10 ms */),
+fVerbosity(verbose), fMsg("TrgSim", verbose)
 {}
 
-TrgSim::TrgSim(const char* signalFilePath, float sigFreqHz, float tDurationSec, const char* noiseFilePath)
-: TrgSim()
+TrgSim::TrgSim(std::string signalFilePath, float sigFreqHz, float tDurationSec, std::string noiseFilePath, unsigned int seed, Verbosity verbose)
+: TrgSim(verbose)
 {
     SetSignalFile(signalFilePath);
-    SetSignalTime(fSigFreqHz, fTDurationSec);
+    SetRandomSeed(seed);
+    SetSignalTime(sigFreqHz, tDurationSec);
     
     SetNoiseFile(noiseFilePath);
 }
 
 TrgSim::~TrgSim() {}
 
-void TrgSim::SetSignalFile(const char* signalFilePath)
+void TrgSim::SetSignalFile(std::string signalFilePath)
 {
     TChain* signalChain = new TChain("data");
-    signalChain->Add(signalFilePath);
+    signalChain->Add(signalFilePath.c_str());
     fSignalTree = signalChain;
     fSignalTree->SetBranchAddress("TQREAL", &fSignalIDTQ);
+    fTotalSignalEntries = fSignalTree->GetEntries();
 }
 
-void TrgSim::SetNoiseFile(const char* noiseFilePath)
+void TrgSim::SetNoiseFile(std::string noiseFilePath)
 {
     TChain* noiseChain = new TChain("data");
-    noiseChain->Add(noiseFilePath);
+    noiseChain->Add(noiseFilePath.c_str());
     fNoiseTree = noiseChain;
     fNoiseTree->SetBranchAddress("TQREAL", &fNoiseIDTQ);
     fNoiseTree->SetBranchAddress("TQAREAL", &fNoiseODTQ);
-}
-
-void TrgSim::SetSignalFrequency(float sigFreqHz)
-{
-    fSigFreqHz = sigFreqHz;
-}
-
-void TrgSim::SetTimeDuration(float tDurationSec)
-{
-    fTDurationSec = tDurationSec;
+    fTotalNoiseEntries = fNoiseTree->GetEntries();
 }
 
 void TrgSim::SetSignalTime(float sigFreqHz, float tDurationSec)
@@ -53,7 +52,25 @@ void TrgSim::SetSignalTime(float sigFreqHz, float tDurationSec)
     SetSignalFrequency(sigFreqHz);
     SetTimeDuration(tDurationSec);
     
+    double dt = 100e-6;
+    unsigned long tAxisSize = (unsigned long)(fTDurationSec/dt);
+    double poissonMeanDt = dt * fSigFreqHz;
     
+    TUnuran poissonGen; poissonGen.SetSeed(fRandomSeed);
+    TRandom3 uniformGen(fRandomSeed);
+    poissonGen.InitPoisson(poissonMeanDt);
+    
+    fSignalTime.clear();
+    for(unsigned long i=0; i<tAxisSize; i++) {
+        unsigned int nOccurrences = poissonGen.SampleDiscr();
+        for (unsigned int occ=0; occ<nOccurrences; occ++)
+            fSignalTime.push_back((i+uniformGen.Rndm())*dt);
+    }
+    
+    fMsg.Print(Form("Seed: %d", fRandomSeed), pDEBUG);
+    fMsg.Print(Form("Signal frequency: %3.2f Hz, Time duration: %3.2f sec", fSigFreqHz, fTDurationSec), pDEBUG);
+    fMsg.Print(Form("Signal occurence: %d, Simulated signal frequency: %3.2f Hz", fSignalTime.size(), fSignalTime.size()/fTDurationSec));
+    fMsg.Print(Form("Total input MC events: %d", fTotalSignalEntries));
 }
 
 void TrgSim::Simulate()
@@ -82,4 +99,22 @@ void TrgSim::Simulate()
         }
         
     }
+}
+
+PMTHitCluster TrgSim::GetSignalHits(double global_t_min, double global_t_max)
+{
+    PMTHitCluster cluster;
+    
+    // get signal entry
+    
+    
+    
+    return cluster;
+}
+
+PMTHitCluster TrgSim::GetNoiseHits(double global_t_min, double global_t_max)
+{
+    PMTHitCluster cluster;
+    
+    return cluster;
 }
--- src/TrgSim/TrgSim.hh
+++ src/TrgSim/TrgSim.hh
@@ -7,18 +7,24 @@
 
 #include "tqrealroot.h"
 
+#include "PMTHitCluster.hh"
+#include "Printer.hh"
+
 class TrgSim
 {
     public:
-        TrgSim();
-        TrgSim(const char* signalFilePath, float sigFreqHz, float tDurationSec, const char* noiseFilePath);
+        TrgSim(Verbosity verbose=pDEFAULT);
+        TrgSim(std::string signalFilePath, float sigFreqHz, float tDurationSec, std::string noiseFilePath, unsigned int seed=0, Verbosity verbose=pDEFAULT);
         ~TrgSim();
         
         // setter
-        void SetSignalFrequency(float sigFreqHz);
-        void SetTimeDuration(float tDuration);
-        void SetSignalFile(const char* signalFilePath);
-        void SetNoiseFile(const char* noiseFilePath);
+        void SetVerbosity(Verbosity verbose) { fVerbosity = verbose; fMsg.SetVerbosity(verbose); }
+        void SetSignalFrequency(float sigFreqHz) { fSigFreqHz = sigFreqHz; }
+        void SetTimeDuration(float tDurationSec) { fTDurationSec = tDurationSec; }
+        void SetRandomSeed(unsigned int seed) { fRandomSeed = seed; }
+        void SetSegmentLength(float segLength) { fSegmentLength = segLength; }
+        void SetSignalFile(std::string signalFilePath);
+        void SetNoiseFile(std::string noiseFilePath);
         
         // signal time generation
         void SetSignalTime(float sigFreqHz, float tDurationSec);
@@ -28,16 +34,27 @@ class TrgSim
         void Simulate();
         
     private:
+        void FillSegement();
+        
+        PMTHitCluster GetSignalHits(double global_t_min, double global_t_max);
+        PMTHitCluster GetNoiseHits(double global_t_min, double global_t_max);
+    
         float fSigFreqHz;
         float fTDurationSec;
+        unsigned int fRandomSeed;
         std::vector<double> fSignalTime; 
         
-        TTree* fSignalTree;
-        TTree* fNoiseTree;
+        TTree *fSignalTree, *fNoiseTree;
+        unsigned int fCurrentSignalEntry, fCurrentNoiseEntry,
+                     fTotalSignalEntries, fTotalNoiseEntries;
         
-        TQReal* fSignalIDTQ;
-        TQReal* fNoiseIDTQ;
-        TQReal* fNoiseODTQ;
+        TQReal *fSignalIDTQ, *fNoiseIDTQ, *fNoiseODTQ;
+        
+        PMTHitCluster fSegment;
+        float fSegmentLength;
+        
+        Verbosity fVerbosity;
+        Printer fMsg;
 };
 
 #endif
