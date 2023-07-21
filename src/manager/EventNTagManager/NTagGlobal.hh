ecbf1636a4ad76d28b2f001bbdab0d4fe609248e
Shota Izumiyama <izumiyama@hep.phys.titech.ac.jp>
2023-07-21 14:16:01 +0900

    Bugfix of name of new option override_...

--- src/manager/EventNTagManager/NTagGlobal.hh
+++ src/manager/EventNTagManager/NTagGlobal.hh
@@ -63,6 +63,6 @@ static std::vector<std::string> gCmdOptions = {"force_flat", "outdata", "write_b
                                                "TMINPEAKSEP", "TMATCHWINDOW",
                                                "TRMSTWIDTH", "INITGRIDWIDTH", "MINGRIDWIDTH", "GRIDSHRINKRATE", "VTXMAXRADIUS",
                                                "E_CUTS", "N_CUTS",
-                                               "print", "commit", "tag", "mode", "force_prompt_vertex", "override_prompt_vertex_more_for_lowe"};
+                                               "print", "commit", "tag", "mode", "force_prompt_vertex", "override_delayed_parameters_more_for_lowe"};
 
 #endif
